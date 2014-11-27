c3f6f56c6ff4dd2c56b5eb70a016dffe21b6732e
Ruben Gamez <ruben.gamez@ed.ac.uk>
2014-11-27 17:38:12 +0000

    Put the correct error code for retry (503)
    Issue edina/fieldtrip-gb#129

--- src/pcapi/dbox_provider.py
+++ src/pcapi/dbox_provider.py
@@ -224,7 +224,7 @@ class DropboxProvider(object):
 
             metadata = self.api_client.put_file(name, fp, overwrite)
         except rest.ErrorResponse as e:
-            if e.status == 507:
+            if e.status == 503:
                 upload_file = lambda: self.upload(name, fp, overwrite)
                 metadata = self.__retry(e, upload_file)
             else:
@@ -249,7 +249,7 @@ class DropboxProvider(object):
             metadata = self.api_client.file_create_folder(path)
         except rest.ErrorResponse as e:
             log.debug(e.status)
-            if e.status == 507:
+            if e.status == 503:
                 create_dir = lambda: self.mkdir(path)
                 metadata = self.__retry(e, create_dir)
             elif e.status == 403:
