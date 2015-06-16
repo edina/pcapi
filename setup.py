108f44f89c3ee58c827845daf508c7bbdce14b81
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-06-16 10:48:33 +0100

    Set version to the 1.3.10 as the API it the same used for the 1.3 family

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pcapi",
-    version="1.4",
+    version="1.3.10",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
